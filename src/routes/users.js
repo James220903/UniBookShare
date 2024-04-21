dd35f9d5d592fdafdbe7e1f5a19b24b242d1b636
james thorley <jamesthorley291@gmail.com>
2024-04-21 22:34:03 +0100

    change code

--- src/routes/users.js
+++ src/routes/users.js
@@ -1,33 +1,39 @@
 const express = require('express');
 const router = express.Router();
+const bcrypt = require('bcrypt');
 
 // Import your user model
- const User = require('../models/User');
+const User = require('../models/User');
 
 // GET all users
 router.get('/', async (req, res) => {
-  try {
-     const users = await User.find();
-     res.json(users);
-    res.send('GET all users');
-  } catch (err) {
-    res.status(500).json({ message: err.message });
-  }
+    try {
+        const users = await User.find();
+        res.json(users); // This sends the response with the users, no need to use res.send after this
+    } catch (err) {
+        res.status(500).json({ message: err.message });
+    }
 });
 
 // POST a new user
 router.post('/', async (req, res) => {
-   const user = new User({
-     name: req.body.name,
-  //   // other properties
-   });
-   try {
-     const newUser = await user.save();
-     res.status(201).json(newUser);
-   } catch (err) {
-     res.status(400).json({ message: err.message });
-   }
-  res.send('POST new user');
+    try {
+        // Hash the password before storing it
+        const hashedPassword = await bcrypt.hash(req.body.password, 10);
+
+        // Create a new user instance with the hashed password and other properties
+        const user = new User({
+            username: req.body.username,
+            email: req.body.email,
+            password: hashedPassword // Make sure to store the hashed password
+        });
+
+        // Save the new user to the database
+        const newUser = await user.save();
+        res.status(201).json({ message: "User created", user: newUser });
+    } catch (err) {
+        res.status(400).json({ message: err.message });
+    }
 });
 
 module.exports = router;
