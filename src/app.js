df4996a61f567fbf79f016b17ee9730405ae17f2
james thorley <jamesthorley291@gmail.com>
2024-04-22 12:54:02 +0100

    testing post login

--- src/app.js
+++ src/app.js
@@ -52,6 +52,11 @@ app.get('/register', (req, res) => {
 app.get('/login', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'Login.html'));
 });
+app.post('/api/users/login', async (req, res) => {
+  console.log(req.body); // Check what data is coming in
+  // Rest of your login logic...
+});
+
 
 
 // Add more route handlers here
